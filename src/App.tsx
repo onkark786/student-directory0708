96596950fd4aee5fb5f1fd51caa74c0ddd94de2f
onkar koshti <okk972688@gmail.com>
2026-02-17 21:52:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+     <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
